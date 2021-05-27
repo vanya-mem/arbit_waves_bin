532b83d0af126c764e29dae7be2eed8b1674232d
vanya_mem <vanya.cozheurov@yandex.ru>
2021-05-27 18:33:47 +0300

    added calcs usdt_for_waves on two exchanges

--- Vanek_waves.py
+++ Vanek_waves.py
@@ -22,7 +22,7 @@ def getting_waves_depth(usdtAmount, Type):
     elif Type == 'sell':
         order_book = order_book['bids']
     else:
-        return 'Type должен быть либо Sell, либо Buy'
+        raise Exception('Type должен быть либо Sell, либо Buy')
     usdt_sum = 0
     waves_sum = 0
     for order in order_book:
@@ -49,7 +49,7 @@ def getting_binance_depth(usdtAmount, Type):
     elif Type == 'sell':
         order_book = order_book['bids']
     else:
-        return 'Type должен быть либо Sell, либо Buy'
+        raise Exception('Type должен быть либо Sell, либо Buy')
     usdt_sum = 0
     waves_sum = 0
     for order in order_book:
@@ -66,5 +66,66 @@ def getting_binance_depth(usdtAmount, Type):
     return waves_sum
 
 
+def calc_usdt_for_waves_bin(wavesAmount, Type):
+    if wavesAmount == str(wavesAmount):
+        wavesAmount = int(wavesAmount)
+    order_book = requests.get('https://api3.binance.com/api/v3/depth?symbol=WAVESUSDT&limit=50').json()
+    Type = Type.lower()
+    if Type == 'buy':
+        order_book = order_book['asks']
+    elif Type == 'sell':
+        order_book = order_book['bids']
+    else:
+        raise Exception('Type должен быть либо Sell, либо Buy')
+    usdt_sum = 0
+    waves_sum = 0
+    for order in order_book:
+        order_price = float(order[0])
+        order_waves_amount = float(order[1])
+        waves_sum_diff = min(wavesAmount - waves_sum, order_waves_amount)
+        waves_sum += waves_sum_diff
+        usdt_sum += (waves_sum_diff * order_price)
+
+        if waves_sum >= wavesAmount:
+            break
+
+    return usdt_sum
+
+
+def calc_usdt_for_waves_wex(wavesAmount, Type):
+    pw.setNode(node='http://nodes.wavesnodes.com', chain='mainnet')
+    pw.setMatcher(node='https://matcher.waves.exchange')
+    waves_asset = pw.Asset(WAVES_ID)
+    usdn_asset = pw.Asset(USDN_ID)
+    asset_pair = pw.AssetPair(waves_asset, usdn_asset)
+    order_book = asset_pair.orderbook()
+
+    if wavesAmount == str(wavesAmount):
+        wavesAmount = int(wavesAmount)
+
+    Type = Type.lower()
+    if Type == 'buy':
+        order_book = order_book['asks']
+    elif Type == 'sell':
+        order_book = order_book['bids']
+    else:
+        raise Exception('Type должен быть либо Sell, либо Buy')
+    usdt_sum = 0
+    waves_sum = 0
+    for order in order_book:
+        order_waves_amount = order['amount'] / 10 ** asset_pair.asset1.decimals
+        order_price = order['price'] / 10 ** (8 + asset_pair.asset2.decimals - asset_pair.asset1.decimals)
+        waves_sum_diff = min(wavesAmount - waves_sum, order_waves_amount)
+        waves_sum += waves_sum_diff
+        usdt_sum += (waves_sum_diff * order_price)
+
+        if waves_sum >= wavesAmount:
+            break
+
+    return usdt_sum
+
+
 print(getting_waves_depth(20000, 'buy'))
-print(getting_binance_depth(usdtAmount=20000, Type='buy'))
+print(getting_binance_depth(usdtAmount=20000, Type='sell'))
+print(calc_usdt_for_waves_bin(1219, 'sell'))
+print(calc_usdt_for_waves_wex(1207, 'sell'))
